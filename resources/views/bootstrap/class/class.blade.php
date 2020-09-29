3986fd9fb52c1760630775773c1140b7f8cde0f5
bathuc <lebathuc@gmail.com>
2020-09-29 09:35:31 +0700

    update bootstrap

--- resources/views/bootstrap/class/class.blade.php
+++ resources/views/bootstrap/class/class.blade.php
@@ -1,6 +1,25 @@
 @extends('layouts.bootstrap')
 
 @section('content')
-    <h2 class="heading"># breakpoint</h2>
-    <p>xs(default), sm, md, lg, xl</p>
+    <h3 class="heading"># breakpoint</h3>
+    <div class="desc">
+        <p><span class="text-red">xs</span>(default), <span class="text-red">sm</span>, <span class="text-red">md</span>, <span class="text-red">lg</span>, <span class="text-red">xl</span></p>
+    </div>
+
+    <h3 class="heading"># spacing</h3>
+    <div class="desc">
+        <p><span class="text-red">m</span>, <span class="text-red">p</span></p>
+        <p><span class="text-red">t</span>, <span class="text-red">r</span>, <span class="text-red">b</span>, <span class="text-red">l</span>, <span class="text-red">x</span>, <span class="text-red">y</span>, blank</p>
+        <p>(<span class="text-red">n</span>), <span class="text-red">0</span>, <span class="text-red">1</span>, <span class="text-red">2</span>, <span class="text-red">3</span>, <span class="text-red">4</span>, <span class="text-red">5</span>, <span class="text-red">auto</span></p>
+    </div>
+
+    <h3 class="heading"># sizing</h3>
+    <div class="desc">
+        <p><span class="text-red">w</span>, <span class="text-red">h</span> - <span class="text-red">25</span>, <span class="text-red">50</span>, <span class="text-red">75</span>, <span class="text-red">100</span>, <span class="text-red">auto</span></p>
+        <p><span class="text-red">mw-100</span>, <span class="text-red">mh-100</span></p>
+        <p>viewport: <span class="text-red">vw-100</span>, <span class="text-red">vh-100</span>, <span class="text-red">min-vw-100</span>, <span class="text-red">min-vh-100</span></p>
+        <p>text-size: class <span class="text-red">h1</span> -> <span class="text-red">h5</span></p>
+    </div>
 @endsection
+
+{{-- <span class="text-red"></span>  --}}
--- resources/views/layouts/bootstrap.blade.php
+++ resources/views/layouts/bootstrap.blade.php
@@ -3,17 +3,9 @@
 @section('body-class', 'document')
 
 @section('body')
-    {{-- nav bar--}}
-    <div class="container-fluid bg-dark">
-        <div class="container d-flex justify-content-between align-items-center">
-            <span class="text-white my-3">Project Name</span>
-            <div></div>
-        </div>
-    </div>
-
     <div class="main-wrapper row">
         {{--side bar--}}
-        <div class="col-2 pt-3 overflow-auto" style="background-color: #F9F9FC; height: calc(100vh - 56px - 1rem)">
+        <div class="col-2 pt-3 overflow-auto" style="background-color: #F9F9FC; height: calc(100vh - 1rem)">
             <ul class="sidebar pl-4">
                 <li>
                     <h5 class="font-weight-bold">Utility</h5>
